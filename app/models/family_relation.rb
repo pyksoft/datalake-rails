01f578e414b0163172d1700cf5ccde6124ec6998
pyksoft <pyksoft@gmail.com>
2015-08-25 16:17:36 +0800

    update

--- app/models/family_relation.rb
+++ app/models/family_relation.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: family_relations
+#
+#  id               :integer          not null, primary key
+#  my_id            :integer
+#  relation_name    :string(255)
+#  relation_id_card :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class FamilyRelation < ActiveRecord::Base
 end
--- spec/factories/family_relations.rb
+++ spec/factories/family_relations.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: family_relations
+#
+#  id               :integer          not null, primary key
+#  my_id            :integer
+#  relation_name    :string(255)
+#  relation_id_card :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 FactoryGirl.define do
   factory :family_relation do
     my_id 1
