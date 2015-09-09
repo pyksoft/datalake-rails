9e97f7fe6747557573d2d660438e36c92e62387e
pyksoft <pyksoft@gmail.com>
2015-09-10 00:24:57 +0800

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ User.create!([
                  {email: "admin@admin.com", password: "12345678", password_confirmation: "12345678"},
                  {email: "dilin.life@gmail.com", password: "12345678", password_confirmation: "12345678"}
              ])
+
+Reservation.create!([
+                   {name: "李浩", notary_type: "forien", reserve_at: Time.now},
+                  {name: "王菲", notary_type: "forien", reserve_at: Time.now}
+               ])
