a215e5bda9c2e94affa120df5e69b3f6f301fee1
pyksoft <pyksoft@gmail.com>
2015-10-17 13:33:32 +0600

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,28 @@
+if User.count == 0
+
+  400.times do |n|
+    id_no = "421081#{Faker::Number.number(12)}"
+    User.create!(id_no: id_no, realname: Faker::Name.name, email: "demo#{n}@gmail.com", password: "12345678", password_confirmation: "12345678")
+  end
 
 
-400.times do |n|
-  id_no = "421081#{Faker::Number.number(12)}"
-  User.create!(id_no: id_no, realname: Faker::Name.name, email: "demo#{n}@gmail.com", password: "12345678", password_confirmation: "12345678")
+  Staff.create!([
+                    {realname: "管理员", role: "admin", email: "admin@gmail.com", password: "12345678", password_confirmation: "12345678"},
+                    {realname: "使用者", role: "user", email: "user@gmail.com", password: "12345678", password_confirmation: "12345678"},
+                    {realname: "审批人", role: "audit", email: "audit@gmail.com", password: "12345678", password_confirmation: "12345678"},
+                    {realname: "录入人", role: "typer", email: "typer@gmail.com", password: "12345678", password_confirmation: "12345678"},
+                ])
+
+  100.times do |n|
+    id_no = "421081#{Faker::Number.number(12)}"
+    Profile.create(realname: Faker::Name.name, id_no: id_no, archive_id: n + 1)
+    Archive.create
+  end
+
+
+  400.times do
+    offset = Faker::Number.between(-5, 5).days + Faker::Number.between(-30, 30).minutes + Faker::Number.between(-12, 12).hours
+    Reservation.create!(user_id: Faker::Number.between(1, User.last.id), reserve_at: Time.now + offset)
+  end
+
 end
-
-
-Staff.create!([
-                 {realname: "管理员", role: "admin", email: "admin@gmail.com", password: "12345678", password_confirmation: "12345678"},
-                 {realname: "使用者", role: "user", email: "user@gmail.com", password: "12345678", password_confirmation: "12345678"},
-                 {realname: "审批人", role: "audit", email: "audit@gmail.com", password: "12345678", password_confirmation: "12345678"},
-                 {realname: "录入人", role: "typer", email: "typer@gmail.com", password: "12345678", password_confirmation: "12345678"},
-             ])
-
-100.times do |n|
-  id_no = "421081#{Faker::Number.number(12)}"
-  Profile.create(realname: Faker::Name.name, id_no: id_no, archive_id: n + 1)
-  Archive.create
-end
-
-
-400.times do
-  offset = Faker::Number.between(-5, 5).days + Faker::Number.between(-30, 30).minutes + Faker::Number.between(-12, 12).hours
-  Reservation.create!(user_id: Faker::Number.between(1, User.last.id), reserve_at: Time.now + offset)
-end
-
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 bundle exec rake db:migrate
+bundle exec rake db:seed
 bundle exec rake assets:precompile
 bundle exec unicorn -p 3000 -c config/unicorn.rb
