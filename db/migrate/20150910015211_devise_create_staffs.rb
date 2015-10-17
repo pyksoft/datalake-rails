c6e7c7ca5fe83fd9a3abc14930b4e3032d8c5e64
pyksoft <pyksoft@gmail.com>
2015-10-17 10:55:20 +0600

    forbidden index for setting temp

--- db/migrate/20150910015211_devise_create_staffs.rb
+++ db/migrate/20150910015211_devise_create_staffs.rb
@@ -34,8 +34,8 @@ class DeviseCreateStaffs < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    add_index :staffs, :email,                unique: true
-    add_index :staffs, :reset_password_token, unique: true
+    #add_index :staffs, :email,                unique: true
+    #add_index :staffs, :reset_password_token, unique: true
     # add_index :staffs, :confirmation_token,   unique: true
     # add_index :staffs, :unlock_token,         unique: true
   end
