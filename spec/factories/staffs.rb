886af753c53325578ba0804727358d74c5770e36
pyksoft <pyksoft@gmail.com>
2015-10-19 16:09:20 +0600

    add user_staff check

--- spec/factories/staffs.rb
+++ spec/factories/staffs.rb
@@ -9,6 +9,21 @@ FactoryGirl.define do
       email "admin@gmail.com"
     end
 
+    factory :audit do
+      role :audit
+      email "audit@gmail.com"
+    end
+
+    factory :typer do
+      role :typer
+      email "typer@gmail.com"
+    end
+
+    factory :user_staff do
+      role :user
+      email "user@gmail.com"
+    end
+
   end
 
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -8,6 +8,15 @@ describe Ability, :type => :model do
     let(:admin) { create(:admin) }
     let(:ability){ Ability.new(admin) }
     it { is_expected.to be_able_to(:manage, Archive) }
+    it { is_expected.to be_able_to(:search, Profile) }
   end
+
+  context "User can search and show" do
+    let(:user_staff) { create(:user_staff) }
+    let(:ability){ Ability.new(user_staff) }
+    it { is_expected.to be_able_to(:show, Archive) }
+    it { is_expected.to be_able_to(:search, Profile) }
+  end
+
 
 end
