8e7e893a60f15b52f67d420f9457a9ddc0638951
pyksoft <pyksoft@gmail.com>
2015-10-19 16:13:41 +0600

    update

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -11,12 +11,28 @@ describe Ability, :type => :model do
     it { is_expected.to be_able_to(:search, Profile) }
   end
 
-  context "User can search and show" do
+  context "UserStaff can search and show" do
     let(:user_staff) { create(:user_staff) }
     let(:ability){ Ability.new(user_staff) }
+    it { is_expected.to be_able_to(:show, Archive) }
+    it { is_expected.not_to be_able_to(:edit, Archive) }
+    it { is_expected.to be_able_to(:search, Profile) }
+  end
+
+  context "Typer can search and show" do
+    let(:typer) { create(:typer) }
+    let(:ability){ Ability.new(typer) }
     it { is_expected.to be_able_to(:show, Archive) }
     it { is_expected.to be_able_to(:search, Profile) }
   end
 
+
+  context "Audit can search and show" do
+    let(:audit) { create(:audit) }
+    let(:ability){ Ability.new(audit) }
+    it { is_expected.to be_able_to(:show, Archive) }
+    it { is_expected.to be_able_to(:edit, Archive) }
+    it { is_expected.to be_able_to(:search, Profile) }
+  end
 
 end
