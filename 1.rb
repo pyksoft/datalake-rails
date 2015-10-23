c8a68b9ef3996e4fdb8801476affd675bc669138
pyksoft <pyksoft@gmail.com>
2015-10-23 20:43:33 +0600

    update

--- 1.rb
+++ 1.rb
@@ -12,7 +12,11 @@ Prawn::Document.generate("hello.pdf") do
   person_info = make_table([["姓名", "    ", "性别", "  ", "出生日期", "      ", "邮箱", "   "], ["现住址", "  ", "电话", "    "]])
   person = make_table([[{:content => "申请人", :colspan => 3}], [person_info]])
 
-  table([["往           国家/地区使用          申请公证用途: 定居/探亲/工作/学习/结婚/其它"], [person], ["工作单位名称, 地址"], ["请在下列需要办理"],
+  table([["往           国家/地区使用          申请公证用途: 定居/探亲/工作/学习/结婚/其它"]])
+  table([["申请人", person_info]])
+  table([["工作单位名称, 地址"], ["请在下列需要办理"]], :column_widths => [540])
+=begin
+  table([[person], ["工作单位名称, 地址"], ["请在下列需要办理"],
           ["申办何种公证", [["xx"], ["yy"]]],
           ["如办出生"],
           ["父母，配偶"],
@@ -24,6 +28,7 @@ Prawn::Document.generate("hello.pdf") do
           ["申请日期: "]
         ])
   #table [["xxeee", "gex"], ["apply", person_table]]
+=end
 end
 
 system("open hello.pdf")
