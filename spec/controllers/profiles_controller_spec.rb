ef58aa30c7bf0d3b4b98f157416cb9e89756cf18
pyksoft <pyksoft@gmail.com>
2015-10-28 20:22:04 +0800

    update

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -5,10 +5,16 @@ RSpec.describe ProfilesController, type: :controller do
   login_admin
   describe "GET #new" do
     it "redirect profile edit" do
-      post :create, profile: {"realname"=>"fe", "id_no"=>"421081198912013474", "email"=>"de"}
+      post :create, profile: {"realname"=>"雷锋", "id_no"=>"421081198912013474", "email"=>"dilin.life@gmail.com"}
       expect(response).to have_http_status(302)
       expect(response).to redirect_to(edit_profile_path(Profile.last))
     end
+
+    it "profile params error" do
+      post :create, profile: {"realname"=>"雷锋xxyy", "id_no"=>"421081198912013474", "email"=>"dilin.life@gmail.com"}
+      expect(response).to have_http_status(200)
+    end
+
   end
 
 end
