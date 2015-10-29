626431498b0316fd052e358cb00786b794d4c360
pyksoft <pyksoft@gmail.com>
2015-10-29 15:33:44 +0800

    add create profiles code

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -29,6 +29,20 @@ RSpec.describe ProfilesController, type: :controller do
       get :new_archive
       expect(response).to have_http_status(200)
     end
+
+    context "POST profiles#create" do
+      it "redirect profile edit" do
+        post :create, profile: {"realname"=>"雷锋", "id_no"=>"421081198912013474", "email"=>"dilin.life@gmail.com"}
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(edit_profile_path(Profile.last))
+      end
+
+      it "profile params error" do
+        post :create, profile: {"realname"=>"雷锋xxyy", "id_no"=>"421081198912013474", "email"=>"dilin.life@gmail.com"}
+        expect(response).to have_http_status(200)
+      end
+    end
+
   end
 
 
