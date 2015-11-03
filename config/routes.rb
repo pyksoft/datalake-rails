03e0d9131314f7a39e2ad2b1a1705b6e905074e6
pyksoft <pyksoft@gmail.com>
2015-11-03 21:21:43 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
   devise_for :staffs, controllers: { passwords: "staffs/passwords", sessions: "staffs/sessions", registrations: "staffs/registrations"}
   post 'members', to: 'staffs#create'
 
-  get "/data/lwnotary-datalake-uploads/(*link)" => redirect("/uploads/%{link}.%{format}")
+  get "/data/lwnotary-datalake-uploads/(*link)" => redirect("/upload_files/%{link}.%{format}")
 
   mount Api::Dispatch => '/api'
   mount RailsSettingsUi::Engine, at: 'settings'
